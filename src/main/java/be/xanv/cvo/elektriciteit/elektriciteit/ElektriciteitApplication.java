45631c2d7a9f91531cf8d2957d911ca390e498a2
Xan Vranckaert <xan.vranckaert@cegeka.com>
2018-10-05 16:12:46 +0200

    setUp

---
+++ src/main/java/be/xanv/cvo/elektriciteit/elektriciteit/ElektriciteitApplication.java
@@ -0,0 +1,12 @@
+package be.xanv.cvo.elektriciteit.elektriciteit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElektriciteitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElektriciteitApplication.class, args);
+	}
+}
---
+++ src/test/java/be/xanv/cvo/elektriciteit/elektriciteit/ElektriciteitApplicationTests.java
@@ -0,0 +1,16 @@
+package be.xanv.cvo.elektriciteit.elektriciteit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ElektriciteitApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
